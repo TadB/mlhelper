2123dc4cdfe57218fcac360068f10f6d31e9e246
acny <tadeusz.brzytwa@gmail.com>
2019-05-04 17:49:21 +0200

    add database models

---
+++ app/models.py
@@ -0,0 +1,19 @@
+from app import db
+
+
+class Image(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    path = db.Column(db.String(128), index=True, unique=True)
+    content_id = db.Column(db.Integer, db.ForeignKey("content.id"))
+
+    def __repr__(self):
+        return f"<Image: {self.path}>"
+
+
+class Content(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.String(128), index=True, unique=True)
+    text = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        return f"<Content from site: {self.url}>"
